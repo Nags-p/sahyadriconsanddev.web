85b579f613dc80bf912eff491bb0a034794af2d0
Nags-p <nagarajup4253@gmail.com>
2025-11-20 15:08:03 +0530

    adf

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 // For best practice, add 'config.js' to your .gitignore file.
 
 const config = {
-    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbz3yjKhrntSZqLAe6NNOHY6sW3bxbQFkB1Dck61Xonjgi8OfQ3YmzwTc0nj-E3J5f8rPQ/exec',
+    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzCTG57hr4rN2ajWQF1jkW4YPrVX1VXiwVRbXPWATtmwi9Sb2fRRHT3NzU2hxlV4c7iGg/exec',
     SUPABASE_URL: 'https://qrnmnulzajmxrsrzgmlp.supabase.co',
     SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFybm1udWx6YWpteHJzcnpnbWxwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzOTg0NTEsImV4cCI6MjA3ODk3NDQ1MX0.BLlRbin09uEFtwsJNTAr8h-JSy1QofEKbW-F2ns-yio'
 };
