546709b0ebfae85a00d5ac3677d4800c8d2874da
Nags-p <nagarajup4253@gmail.com>
2025-12-11 13:07:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,9 +9,4 @@ const config = {
     SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwnR0cbDa0oNp_T9BPBXyB-stCzH2zGogbMisdcDigtPl9HOrqoL06W7Oxn_rwlTlkgvA/exec',
     SUPABASE_URL: 'https://qrnmnulzajmxrsrzgmlp.supabase.co',
     SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFybm1udWx6YWpteHJzcnpnbWxwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzOTg0NTEsImV4cCI6MjA3ODk3NDQ1MX0.BLlRbin09uEFtwsJNTAr8h-JSy1QofEKbW-F2ns-yio'
-
-    name: "Sahyadri Construction & Developers",
-    phone: "+91 9876543210",
-    email: "info@sahyadriconstruction.in",
-    address: "Bengaluru, Karnataka"
 };
